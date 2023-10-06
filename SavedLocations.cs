30e16a8754106ea34582813713d01b719f72e8b6
BitForgeCraftedCode <11381086+BitForgeCraftedCode@users.noreply.github.com>
2023-10-06 16:29:57 -0400

    add location id to SavedLocations class

--- SavedLocations.cs
+++ SavedLocations.cs
@@ -6,12 +6,14 @@ namespace OpenWeatherMap
         private string _city;
         private string _stateCode;
         private string _countryCode;
+        private string _locationId;
 
-        public SavedLocations(string city = "", string stateCode = "", string countryCode = "")
+        public SavedLocations(string city = "", string stateCode = "", string countryCode = "", string locationID = "")
         {
             _city = city;
             _stateCode = stateCode;
             _countryCode = countryCode;
+            _locationId = locationID;
         }
         public string City 
         { 
@@ -31,5 +33,10 @@ namespace OpenWeatherMap
             set { _countryCode = value; }
         }
 
+        public string LocationId
+        {
+            get { return _locationId; }
+            set { _locationId = value; }  
+        }
     }
 }
