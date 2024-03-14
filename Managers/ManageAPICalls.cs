a7d16e95d2477dc9e76e14dac13825bf23d16d62
BitForgeCraftedCode <11381086+BitForgeCraftedCode@users.noreply.github.com>
2024-03-14 13:57:55 -0400

    add some docs to ManageAPICalls class

--- Managers/ManageAPICalls.cs
+++ Managers/ManageAPICalls.cs
@@ -3,7 +3,10 @@ using Spectre.Console;
 using System.Net.Http.Headers;
 using System.Text.Json;
 
-
+/* using Free API: https://openweathermap.org/price 
+ * to get CurrentWeather or 3-hour Forecast 5 days we need to make a call to the Geocoding API first to convert City, State, Country to Lat Lon coords
+ * App will have user enter at least one default location before any api call made -- City, State, Country and this will be saved in database -- uses SavedLocations class to map data
+ */
 namespace OpenWeatherMap.Managers
 {
     internal static class ManageAPICalls
@@ -13,6 +16,13 @@ namespace OpenWeatherMap.Managers
         private static readonly HttpClient client = new HttpClient();
 
         private static int currentLocationId;
+
+        /*
+         * end point docs https://openweathermap.org/api/geocoding-api -- this gets lat lon from user City, State, Country input
+         * use the geo api endpoint to get the location for USA or notUS
+         * save the location Text -- app state
+         * return the fetched Location
+         */
         private static async Task<List<Location>> GetLatLongCoordsAsync(HttpClient client, SavedLocations locationForWeather, bool forCurrentWeather)
         {
             string json;
@@ -43,6 +53,8 @@ namespace OpenWeatherMap.Managers
                     ManageSavedWeatherText.SaveForecastLocationText(json);
                 }
 
+                //https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/operators/null-coalescing-operator
+                // the ?? new() resolves teh nullable warning
                 location = JsonSerializer.Deserialize<List<Location>>(json) ?? new();
             }
 
@@ -74,8 +86,24 @@ namespace OpenWeatherMap.Managers
 
         }
 
+        /*
+         * This is the public method that will call GetLatLongCoordsAsync to get the location and lat lon needed for Weather and Forecast
+         * if SavedLocation in database already has lat lon GetLatLongCoordsAsync will not be called 
+         * 
+         * 1. GetSavedLocations from database
+         * 2. if defaultLocation true get the default location and set it equal to locationForWeather
+         *    else get the location at the atLocationId and set it equal to locationForWeather  
+         * 3. set the currentLocationId variable equal to the locationForWeather Id -- used to SaveCurrentWeather to database
+         * 4. to prevent additional api calls if DB value locationForWeather has lat lon add locationForWeather to location list and save location text -- app state
+         *    else get lat lon from api GetLatLongCoordsAsync and save lat lon to DB
+         * 5. return the Location List -- length always 1
+         * 
+         * Note: GetLatLongCoordsAsync saves Location Text
+         */
         public static async Task<List<Location>> GetLocation(bool forCurrentWeather, bool defaultLocation, int? atLocationId = null)
         {
+            //limit of 1 on api call so this location List will always have length of 1
+            //new list each time -- length always 1
             List<Location> location = new List<Location>();
 
             List<SavedLocations> savedLocationsList = ManageSQL.GetSavedLocations();
@@ -127,6 +155,7 @@ namespace OpenWeatherMap.Managers
                     ManageSavedWeatherText.SaveForecastLocationText(json);
                 }
             }
+            //app state saved in GetLatLongCoordsAsync() so dont need to save text below
             else
             {
                 client.DefaultRequestHeaders.Accept.Clear();
@@ -147,6 +176,12 @@ namespace OpenWeatherMap.Managers
             return location;
         }
 
+        /*
+         * end point docs https://openweathermap.org/current
+         * use the current weather endpoint to get the weather at the specific lat lon
+         * save current weather to Text -- app state
+         * return the fetched weather
+         */
         private static async Task<CurrentWeather> GetCurrentWeatherAsync(HttpClient client, string lat, string lon)
         {
             string json = await client.GetStringAsync($"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=imperial&appid={apiKey}");
@@ -212,6 +247,12 @@ namespace OpenWeatherMap.Managers
 
         }
 
+        /*
+         * This is the public method that will call GetCurrentWeatherAsync to get weather at specific lat lon coords
+         * 1. get current weather 
+         * 2. save current weather to database
+         * 3. return currentWeather
+         */
         public static async Task<CurrentWeather> GetCurrentWeather(List<Location> location)
         {
             CurrentWeather currentWeather = null;
@@ -232,6 +273,12 @@ namespace OpenWeatherMap.Managers
             return currentWeather;
         }
 
+        /*
+         * end point docs https://openweathermap.org/forecast5
+         * use the forecast endpoint to get the 5 day forecast at the specific lat lon
+         * save the current forecast to Text -- app state
+         * return the fetched forecast
+         */
         private static async Task<ForecastWeather> GetForecastAsync(HttpClient client, string lat, string lon)
         {
             string json = await client.GetStringAsync($"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&units=imperial&appid={apiKey}");
@@ -248,6 +295,13 @@ namespace OpenWeatherMap.Managers
             return forecastWeather;
         }
 
+        /*
+         * The is the public method that will call GetForecastAsync to get the forecast at specific lat lon coords
+         * 1. get current forecast
+         * 2. return current forecastWeather 
+         * 
+         * Note: Forecast is not saved to database
+         */
         public static async Task<ForecastWeather> GetForecast(List<Location> location)
         {
             ForecastWeather forecastWeather = null;
